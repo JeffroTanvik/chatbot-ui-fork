ff39f370c057bffeca6d2775ee27b4527efab13c
Jeffro <JeffroTanvik@gmail.com>
2023-05-04 01:54:28 +0800

    Update Chat.tsx

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -352,12 +352,12 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
       {!(apiKey || serverSideApiKeyIsSet) ? (
         <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
           <div className="text-center text-4xl font-bold text-black dark:text-white">
-            Welcome to Jᥱffr᧐'s ChatGPT
+            Welcome to Jeffro ChatGPT
           </div>
           <div className="text-center text-lg text-black dark:text-white">
             <div className="mb-8">{`Jᥱffr᧐'s ChatGPT is an open source clone of OpenAI's ChatGPT UI.`}</div>
             <div className="mb-2 font-bold">
-              Important: Jᥱffr᧐'s ChatGPT is 100% unaffiliated with OpenAI.
+              Important: Jeffro ChatGPT is 100% unaffiliated with OpenAI.
             </div>
           </div>
           <div className="text-center text-gray-500 dark:text-gray-400">
