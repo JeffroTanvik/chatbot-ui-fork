cbe118c1cace6834ff9bb2f982cd96d8eef513d8
Jeffro <JeffroTanvik@gmail.com>
2023-05-04 01:49:33 +0800

    Update Chat.tsx

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -352,22 +352,21 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
       {!(apiKey || serverSideApiKeyIsSet) ? (
         <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
           <div className="text-center text-4xl font-bold text-black dark:text-white">
-            Welcome to Chatbot UI
+            Welcome to Jᥱffr᧐'s ChatGPT
           </div>
           <div className="text-center text-lg text-black dark:text-white">
-            <div className="mb-8">{`Chatbot UI is an open source clone of OpenAI's ChatGPT UI.`}</div>
+            <div className="mb-8">{`Jᥱffr᧐'s ChatGPT is an open source clone of OpenAI's ChatGPT UI.`}</div>
             <div className="mb-2 font-bold">
-              Important: Chatbot UI is 100% unaffiliated with OpenAI.
+              Important: Jᥱffr᧐'s ChatGPT is 100% unaffiliated with OpenAI.
             </div>
           </div>
           <div className="text-center text-gray-500 dark:text-gray-400">
             <div className="mb-2">
-              Chatbot UI allows you to plug in your API key to use this UI with
-              their API.
-            </div>
-            <div className="mb-2">
               It is <span className="italic">only</span> used to communicate
               with their API.
+            </div>
+            <div className="mb-2">
+              And this website is <span className="italic">only</span> for <a href="https://twitter.com/JeffroTanvik">@Jeffro</a> and his friends. 
             </div>
             <div className="mb-2">
               {t(
